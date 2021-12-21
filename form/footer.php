20737827856bb68374eedd39e2d7b82a99486c07
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-12-21 10:41:43 +0700

    form

---
+++ form/footer.php
@@ -0,0 +1,13 @@
+<footer id="footer">
+        <!--Footer-->
+
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <!-- <p class="pull-left"><a href="./Login/logout.php">đăng nhập</a></p> -->
+                    <p class="pull-center">Designed by G11</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/Footer-->
